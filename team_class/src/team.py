5a0739cec5aab91745c4756eff4549766daeeab4
Kenny Wands <kjwands@gmail.com>
2022-05-23 15:48:00 +0100

    formatting team.py

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -1,13 +1,11 @@
 class Team:
     def __init__(self, input_team_name, input_team_players, input_coach):
-
         self.name = input_team_name
         self.players = input_team_players
         self.coach = input_coach
         self.points = 0
 
     def add_player(self, new_player):
-
         self.players.append(new_player)
 
     def has_player(self, player_name):
